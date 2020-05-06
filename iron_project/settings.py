e8b28e4ce46651f58d3b1421cff03e50ad03b482
vk <vinaykumar1908@gmail.com>
2020-05-06 19:27:03 +0530

    latest ready to deploy

--- iron_project/settings.py
+++ iron_project/settings.py
@@ -16,6 +16,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+ENVIRONMENT = os.environ.get('ENVIRONMENT', default='development')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
